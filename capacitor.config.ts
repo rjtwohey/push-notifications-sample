4f157b6ea3ccca4ec5fa0460bd7c808d93652c75
rtwohey <rtwohey@ionic.io>
2025-01-23 11:34:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'push-notifications-sample',
+  webDir: 'www'
+};
+
+export default config;
